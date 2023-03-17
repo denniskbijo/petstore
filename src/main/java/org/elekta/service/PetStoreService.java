fae68b8b721328b609c07cb912523e5c7b254579
denniskbijo <denniskbijo7@gmail.com>
2023-03-17 23:42:29 +0000

    PSv1: Add displayAnimals method

--- src/main/java/org/elekta/service/PetStoreService.java
+++ src/main/java/org/elekta/service/PetStoreService.java
@@ -1,4 +1,22 @@
 package org.elekta.service;
 
+import lombok.extern.java.Log;
+import org.elekta.model.Animal;
+
+import java.util.List;
+
+@Log
 public class PetStoreService {
+
+    /**
+     * Displays the given list of animals using the log.
+     *
+     * @param animals the list of animals to display
+     */
+    public void displayAnimals(List<Animal> animals) {
+        // Display the sorted animals
+        for (Animal animal : animals) {
+            log.info(animal.toString());
+        }
+    }
 }
