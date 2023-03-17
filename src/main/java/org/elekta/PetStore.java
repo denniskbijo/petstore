b5230b9fb9af2164b6e44e6f1cd9d70e58ebbb34
denniskbijo <denniskbijo7@gmail.com>
2023-03-17 16:48:04 +0000

    PSv1: Add PetStore Main class

---
+++ src/main/java/org/elekta/PetStore.java
@@ -0,0 +1,35 @@
+package org.elekta;
+
+import org.elekta.exception.InvalidAnimalTypeException;
+import org.elekta.model.Animal;
+import org.elekta.util.PetConstants;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Represents a pet store that holds a list of animals.
+ */
+public class PetStore implements PetConstants {
+
+
+    /**
+     * The main method that creates a list of animals and prints out information about them.
+     *
+     * @param args the command-line arguments
+     * @throws InvalidAnimalTypeException when invalid animal type is used
+     */
+    public static void main(String[] args) throws InvalidAnimalTypeException {
+        List<Animal> animals = new ArrayList<>();
+        animals.add(Animal.createAnimal(DOG, "John Smith", "Fido", 7));
+        animals.add(Animal.createAnimal(CAT, "John Doe", "Fluffy", 12));
+        animals.add(Animal.createAnimal(BIRD, "Jane Doe", "Jonathon Livingston", 3));
+        animals.add(Animal.createAnimal(SNAKE, "Jack Sack", "Slitherer", 1));
+
+        // TODO add more animals here
+
+        for (Animal animal : animals) {
+            System.out.println(animal);
+        }
+    }
+}
